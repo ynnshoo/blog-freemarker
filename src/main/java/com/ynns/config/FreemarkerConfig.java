bb90523a0f5854e102679a11cf139aed3e42eff4
ynns <zhaowuyan_5@163.com>
2022-05-10 07:25:28 +0800

    freemarker解决日期显示据今时间

---
+++ src/main/java/com/ynns/config/FreemarkerConfig.java
@@ -0,0 +1,18 @@
+package com.ynns.config;
+
+import com.ynns.template.TimeAgoMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class FreemarkerConfig {
+    @Autowired
+    private freemarker.template.Configuration configuration;
+
+    @PostConstruct
+    public void setUp(){
+        configuration.setSharedVariable("timeAgo", new TimeAgoMethod());
+    }
+}
